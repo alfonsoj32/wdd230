90e8d7cebdd106057ad8bee51ae69e8b0e10714b
AlfonsoTumbokon <tum21004@byui.edu>
2024-09-21 19:35:23 -0600

    01 Assignment: Course Home Page

---
+++ scripts/getdates.js
@@ -0,0 +1,5 @@
+// Populate the current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Populate last modified date
+document.getElementById('lastModified').textContent = "Last Modified: " + document.lastModified;
