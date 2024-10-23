b40c44e63077b61bb325ee2204efb062caf69745
AlfonsoTumbokon <tum21004@byui.edu>
2024-10-23 13:42:42 -0600

    updated

---
+++ chamber/scripts/year.js
@@ -0,0 +1,6 @@
+
+// Update the current year
+document.getElementById('currentYear').textContent = new Date().getFullYear();
+
+// Update the last modified date
+document.getElementById('lastModified').textContent = document.lastModified;
